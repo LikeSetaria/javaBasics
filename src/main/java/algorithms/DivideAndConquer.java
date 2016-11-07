ba5be84b16b94b63675a87525a22f8bdba9a1c2a
LikeSetaria <zbc_210@163.com>
2016-11-07 18:04:22 +0800

    MaxMIn

--- src/main/java/algorithms/DivideAndConquer.java
+++ src/main/java/algorithms/DivideAndConquer.java
@@ -6,6 +6,7 @@ package algorithms;
  */
 public class DivideAndConquer {
     /**
+     * No 1
      * 普通的方法求一个数组中的最大值最小值
      * 时间复杂度是2n-2
      * @param a
@@ -23,9 +24,9 @@ public class DivideAndConquer {
     }
 
     /**
+     * No 1
      * 基于分治法，同时求一个数组中的的最大值及最小值，
      * 时间复杂率是（3n-2）/2-2；注意java中值传递以及引用传递，如现在需要
-     * @param args
      */
     public static void max_min(int []a,int left,int right,int []maxnum,int []minnum)
     {
@@ -82,15 +83,65 @@ public class DivideAndConquer {
         //System.out.println("最小值: "+minnum);
 
     }
+
+    /**
+     * No 2
+     * 基于迭代法，实现二分查找
+     *@param sortArr  一个排好序的数组
+     *@param target  待查找的数据
+     *@return 找到返回此target，找不到返回0
+     */
+    public  static int  binarySearchByIteration(int sortArr[],int target){
+        int mid ,low ,high;
+        low=0;high=sortArr.length-1;
+
+        while(low<=high){
+            mid=(high+low)/2;
+            if(sortArr[mid]<target){
+                low=mid+1;
+            }
+            else if (sortArr[mid]>target){
+                high=mid-1;
+            } else return target;
+        }
+        return -1;
+    }
+    /**
+     * No 2
+     *
+     * 基于递归法，实现二分查找
+     *@param sortArr 已经排序好的数组
+     *@param target 查找目标
+     *@param high 数组上标
+     *@param low 数组下标
+     */
+    public  static int  binarySearchByRecursion(int sortArr[],int target,int low ,int high){
+
+        if(low>high)
+            return -1;
+        else{
+            int  mid=(low+high)/2;
+            if(target==sortArr[mid])
+                return target;
+            else  if(target<sortArr[mid])
+                return binarySearchByRecursion(sortArr,target,low,mid-1);
+            return binarySearchByRecursion(sortArr,target,mid+1,high);
+
+        }
+
+    }
     public static void main(String[]args){
         int a[]={4,5,45,956,445,2,364,4,0,2,12};
-        int array[]={1,1,1,1,1};
+        int array[]={1,1,1,1,1,2,3};
+
         int []Max=new int[1];  //java对于实参传递，是采用值传递，所以要改成数组，不能直接使用变量去计算最大值和最小值。
         int []Min=new int[1];
         max_min(array,0,array.length-1,Max,Min);
-
         System.out.println("最大值： "+Max[0]);
         System.out.println("最小值： "+Min[0]);
+
+        int result=binarySearchByRecursion(array,3,0,array.length);
+        System.out.println(result);
 
     }
 }
